4a112e6d6aa59e0249cd3f613c7333daec8751ff
Carlos Silvestri <carlossilvestri9@gmail.com>
2021-01-30 11:47:38 -0400

    Updates

---
+++ precache-manifest.e69101854dc6def1d6fd46a8706fc51f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce7e42d8eb779208ad0c89a18a7a99e",
+    "url": "/react-star-wars/index.html"
+  },
+  {
+    "revision": "5b06b96f4e136b1a0b55",
+    "url": "/react-star-wars/static/css/main.6851758f.chunk.css"
+  },
+  {
+    "revision": "63aa448b2eb2717a8491",
+    "url": "/react-star-wars/static/js/2.bfd1ca47.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-star-wars/static/js/2.bfd1ca47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b06b96f4e136b1a0b55",
+    "url": "/react-star-wars/static/js/main.7132f4ce.chunk.js"
+  },
+  {
+    "revision": "0eb1b7495754e8fce3a2",
+    "url": "/react-star-wars/static/js/runtime-main.b920a414.js"
+  },
+  {
+    "revision": "4ad927dd2314ccd537952903dcf652a3",
+    "url": "/react-star-wars/static/media/buscar-vale.4ad927dd.svg"
+  },
+  {
+    "revision": "3782173c31a881a63fee00165ff87121",
+    "url": "/react-star-wars/static/media/regresar-flecha.3782173c.svg"
+  }
+]);
